02441aa5013480817ae1affb7863480a550066e1
Nitin Sahu <sahunitin970@gmail.com>
2023-07-11 15:25:49 +0530

    Hero Section completed

---
+++ app.js
@@ -0,0 +1,27 @@
+const t1 = gsap.timeline();
+
+t1.from('#nav #logo, #nav ul, #nav #burger', {
+    y: -100,
+    duration: 1,
+    delay: 0.5,
+    stagger: 0.5,
+    opacity: 0
+})
+t1.from('#page-1 #text-section h1', {
+    y: 100,
+    duration: 1,
+    stagger: 0.5,
+    opacity: 0,
+    color: 'black'
+})
+t1.from('#text-section #tech img', {
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    stagger: 0.5
+})
+t1.from('#profile-img', {
+    x: 200,
+    duration: 1,
+    opacity: 0
+})
